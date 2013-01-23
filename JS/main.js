680daee14042fd0ae20514edf345fb4c92905e23
Pedro Rodriguez <pjrod@fullsail.edu>
2013-01-23 14:41:17 -0500

    replaced items, updated js file, corrected some items that were not complete. updated additem.html also.

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,151 @@ VFW Project 2
 */
 window.addEventListener("DOMContentLoaded", function(){
 	
+	function $(x){
+		var elemtID = document.getElementById(x);
+		return elemtID;
+	}
+	
+		
+	//create selec field element with options
+	function activities(){
+		var yourActivity = document.getElementsByTagName("form"),
+			selectItems = $("select"),
+			makeSelections = document.createElement("select");
+			makeSelections.setAttribute("id", "activity");
+		for(var i=0, j=typeOfActivity.length; i<j; i++){
+			var myOptions = document.createElement("option");
+			var optionText = typeOfActivity[i];
+			myOptions.setAttribute("value", optionText);
+			myOptions.innerHTML = optionText;
+			makeSelections.appendChild(myOptions);
+	
+		}
+		selectItems.appendChild(makeSelections);
+		
+		
+	}
+	function flavor(){
+		var myFlavor = document.getElementsByTagName("form"),
+			selectFlavor = $("flavorType"),
+			makeFlavorSel = document.createElement("select");
+			makeFlavorSel.setAttribute("id", "flavorGroup");
+		for(var a=0, b=chooseFlavor.length; a<b; a++){
+			var myFlavorOptions = document.createElement("option");
+			var flavorOptionText = chooseFlavor[a];
+			myFlavorOptions.setAttribute("value", flavorOptionText);
+			myFlavorOptions.innerHTML = flavorOptionText;
+			makeFlavorSel.appendChild(myFlavorOptions);
+			
+		}
+		selectFlavor.appendChild(makeFlavorSel);
+	}
+	function transportation(){
+		var deliveryType = document.getElementsByTagName("form"),
+			myDeliveryMethod = $("pickupType"),
+			makeDeliverySel = document.createElement("select");
+			makeDeliverySel.setAttribute("id", "deliveryType");
+		for(var c=0, d=selectPickupType.length; c<d; c++){
+			var myDeliveryOptions = document.createElement("option");
+			var myDeliveryOptionText = selectPickupType[c];
+			myDeliveryOptions.setAttribute("value", myDeliveryOptionText);
+			myDeliveryOptions.innerHTML = myDeliveryOptionText;
+			makeDeliverySel.appendChild(myDeliveryOptions);
+			
+			
+		}
+			myDeliveryMethod.appendChild(makeDeliverySel);
+	}
+	//find value of selected radio
+	function getRadio(){
+		var radio = document.forms[0].formtype;
+		for(var e=0; e<radio.length; e++){
+			if (radio[e].checked){
+				formTypeValue = radio[e].value;
+			}
+			
+		}
+	}
+	
+	function myControls(n){
+		switch(n){
+			case "on":
+				$("formPartOne").style.display = "none";
+				$("proceed").style.display = "none";
+				$("reset").style.display = "inline";
+				$("displayData").style.display = "none";
+				$("anotherRequest").style.display = "inline";
+				break;
+			case "off":
+				$("formPartOne").style.display = "block";
+				$("proceed").style.display = "block";
+				$("reset").style.display = "inline";
+				$("displayData").style.display = "inline";
+				$("anotherRequest").style.display = "none";				
+				$("items").style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+	
+	//to store the data
+	function proceedWithForm(){
+		var id = Math.floor(Math.random()*10000001);
+		getRadio();
+		var item 				= {};
+			item.formType		= ["Choose Type of Form: ", formTypeValue];
+			item.activity 		= ["Activity: ", $("activity").value];
+			item.firstName 		= ["First Name: ", $("firstName").value];
+			item.yourLastName 	= ["Last Name: ", $("lastName").value];
+			item.email 			= ["Email: ", $("email").value];
+			item.flavor			= ["Flavor Type: ", $("flavorGroup").value];
+			item.howmany		= ["How Many People? ", $("rangeSlider").value];
+			item.delivery  		= ["Delivery Type: ", $("deliveryType").value];
+			item.date			= ["Date to Schedule: ", $("duedate").value];
+			item.addinfo		= ["Additional Information: ", $("moreInfo").value];
+			
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Form Saved and Proceeding");
+	}
+	function getData(){
+		myControls("on");
+		if (localStorage.length ===0){
+			alert("You Have Not Added Any Data!!");
+		}
+		var makeDiv = document.createElement("div");
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement("ul");
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$("items").style.display = "block";
+		for(var i=0, stoLen=localStorage.length; i<stoLen; i++){
+			var makeAList = document.createElement("li");
+			makeList.appendChild(makeAList);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var theinfo = JSON.parse(value);
+			var makeASubList = document.createElement("ul");
+			makeAList.appendChild(makeASubList);
+			for(var n in theinfo){
+				var makeSubListItem = document.createElement("li");
+				makeASubList.appendChild(makeSubListItem);
+				var optSubText = theinfo[n][0]+" "+ theinfo[n][1];
+				makeSubListItem.innerHTML = optSubText;
+			}
+		}
+	}
+	
+	//Variables and function calls
+	var typeOfActivity = ["--Choose The Activity Type--", "Birthdays", "Graduation", "School Activity", "Adult Birthdays", "Weddings", "Bachelor Party", "Bachelorette Party", "Baby Shower", "Job Related", "Going Away", "Get Well", "Other"];
+	var chooseFlavor = ["--Choose The Flavor--", "Flavor 1", "Flavor 2", "Flavor 3"];
+	var selectPickupType= ["--Choose Your Delivery Method--", "Delivered", "Self Pickup"];
+	var formTypeValue;
+
+	activities();
+	flavor();
+	transportation();
+	
 	
 	var placeOrderRadio = document.getElementById("placeOrder");
 	var informationRadio = document.getElementById("information");
@@ -19,29 +164,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	var pickaDate = document.getElementById("duedate");
 	var additionalInfo = document.getElementById("moreInfo");
 	var resetForm = document.getElementById("reset");
-	
-	var captureData = function(){
-		localStorage.setItem("Place Order", placeOrderRadio.value);
-		localStorage.setItem("Information", informationRadio.value);
-		localStorage.setItem("Subscribe", subscribeRadio.value);
-		localStorage.setItem("First Name", yourName.value);
-		localStorage.setItem("Last Name", lastName.value);
-		localStorage.setItem("Email", yourEmail.value);
-		localStorage.setItem("Activity Type", activityType.value);
-		localStorage.setItem("Flavor Type", flavorType.value);
-		localStorage.setItem("Quantity", quantity.value);
-		localStorage.setItem("Type of Pickup", pickupType.value);
-		localStorage.setItem("Date", pickaDate.value);
-		localStorage.setItem("More Information", additionalInfo.value);
-		localStorage.setItem(id, JSON.stringify)
-	};
-	
-	var getData	= function(){
-		var yourNameKey = localStorage.key("First Name");
-		var yourNameValue = localStorage.getItem(yourNameKey);
-		yourName.value = yourNameValue;
-	}
-	
+		
 	var clearForm = function(){
 		if (localStorage.length === 0){
 			alert("You have nothing to clear.")
@@ -53,26 +176,14 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 		
 	}
+	//Buttons with event listeners
+	var displayData = $("displayData");
+	displayData.addEventListener("click", getData);
+	var clearMyform = $("reset");
+	clearMyform.addEventListener("click", clearForm);	
+	var proceed = $("proceed");
+	proceed.addEventListener("click", proceedWithForm);
 	
-	//event listeners
-	placeOrderRadio.addEventListener("blur", captureData);
-	informationRadio.addEventListener("blur", captureData);
-	subscribeRadio.addEventListener("blur", captureData);
-	yourName.addEventListener("blur", captureData);
-	lastName.addEventListener("blur", captureData);
-	yourEmail.addEventListener("blur", captureData);
-	activityType.addEventListener("blur", captureData);
-	flavorType.addEventListener("blur", captureData);
-	quantity.addEventListener("blur", captureData);
-	pickupType.addEventListener("blur", captureData);
-	pickaDate.addEventListener("blur", captureData);
-	additionalInfo.addEventListener("blur", captureData);
-	resetForm.addEventListener("click", clearForm);
-
-
-
-
-
 });
 
 
